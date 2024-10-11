92607862f1cfffc5aa385079cdec73f57e2bee0d
Farhan-000 <farhankhan223316@gmail.com>
2024-10-12 00:41:57 +0500

    Variable in java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
   public static void main (String[] args) {
     String name = "Farhan";
     System.out.println("Hello, "+name);
-    // System.out.println(name);
+    System.out.println(name);
   }
 }
